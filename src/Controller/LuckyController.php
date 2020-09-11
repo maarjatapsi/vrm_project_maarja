bfe5d0dc31011eb282349c987256bff3d9a51e73
Maarja Tapsi <maarja.tapsi@gmail.com>
2020-09-11 10:40:21 +0300

    Symfony tutorial - Rendering a Template

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -5,8 +5,9 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class LuckyController
+class LuckyController extends AbstractController
 {
     /**
      * @Route("/lucky/number")
@@ -15,8 +16,8 @@ class LuckyController
     {
         $number = random_int(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('/number.html.twig', [
+            'number' => $number,
+        ]);
     }
 }
